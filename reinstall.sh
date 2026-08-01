69fd59d0e19aaa6999e4b905ce809e5c1c88a1dd
w9in2er <160832124+w9in2er@users.noreply.github.com>
2026-08-01 10:45:38 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -6,8 +6,8 @@
 # alpine 默认没有 bash，因此 shebang 用 sh，再 exec 切换到 bash
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
+confhome=https://raw.githubusercontent.com/w9in2er/reinstall/main
+
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 用于判断 reinstall.sh 和 trans.sh 是否兼容
